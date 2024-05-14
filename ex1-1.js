36c58fe9d3db37b28a83b2102b6ad2c61352e231
teeerapong2020 <teerapong.chue@gmail.com>
2024-05-14 14:15:44 +0700

    finish

---
+++ ex1-1.js
@@ -0,0 +1,7 @@
+function iloveyou(i,you){
+    return i+you;
+}
+const i=1
+const you=1
+const baby = iloveyou(i,you);
+console.log('My Baby has '+ baby);
---
+++ ex1-2.js
@@ -0,0 +1,6 @@
+function ilove(thing,where){
+    
+}
+const thing = 'you'
+const where = 'everyday'
+console.log('i love '+thing +' '+ where)
